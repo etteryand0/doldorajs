41b35068b041489ef98a25b901ccf034853a8dc8
etteryand0 <etteryand@gmail.com>
2021-07-31 22:29:57 +0900

    :white_check_mark: Add regex test. Might add check_arguments test next

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod error;
+mod tests;
 
 use error::{DError, ErrorCode};
 
---
+++ src/tests/regexp.rs
@@ -0,0 +1,23 @@
+use regex::Regex;
+
+#[test]
+fn test_regex() {
+  let expression = r"^([^a-z])|[^a-z_0-9-]+?";
+  let project_name_re = Regex::new(expression).unwrap();
+  assert_eq!(project_name_re.is_match("1fail_fail"), true);
+  assert_eq!(project_name_re.is_match("5ыщь_some"), true);
+  assert_eq!(project_name_re.is_match("ыодлоывад"), true);
+  assert_eq!(project_name_re.is_match("ASsdfk_sdf"), true);
+  assert_eq!(project_name_re.is_match("+project"), true);
+
+  assert_eq!(project_name_re.is_match("project+new"), true);
+  assert_eq!(project_name_re.is_match("myapp?s"), true);
+  assert_eq!(project_name_re.is_match("mya&zs"), true);
+  assert_eq!(project_name_re.is_match("myaыаZS"), true);
+
+  assert_eq!(project_name_re.is_match("myapp"), false);
+  assert_eq!(project_name_re.is_match("my-app"), false);
+  assert_eq!(project_name_re.is_match("my-app_"), false);
+  assert_eq!(project_name_re.is_match("hello_world"), false);
+  assert_eq!(project_name_re.is_match("hello_my_world"), false);
+}
